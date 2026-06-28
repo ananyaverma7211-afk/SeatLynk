cc58a59957b12049a41858e007204d9c4c6a0d80
ananyaverma7211-afk <ananyaverma7211@gmail.com>
2026-06-28 13:24:02 +0530

    Added search and delete functionality

--- src/pages/BuyTicket.jsx
+++ src/pages/BuyTicket.jsx
@@ -1,16 +1,53 @@
+import { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 
 function BuyTicket() {
-    const tickets = JSON.parse(localStorage.getItem("tickets")) || [];
+   const [tickets, setTickets] = useState([]);
+
+useEffect(() => {
+  const savedTickets =
+    JSON.parse(localStorage.getItem("tickets")) || [];
+
+  setTickets(savedTickets);
+}, []);
+    const [search, setSearch] = useState("");
+    const handleDelete = (pnr) => {
+  const updatedTickets = tickets.filter(
+    (ticket) => ticket.pnr !== pnr
+  );
+
+  setTickets(updatedTickets);
+
+  localStorage.setItem(
+    "tickets",
+    JSON.stringify(updatedTickets)
+  );
+
+  alert("🗑 Ticket Deleted Successfully!");
+};
+
   return (
     <>
       <Navbar />
 
       <section className="buy-page">
         <h1>Available Tickets</h1>
+        <input
+  type="text"
+  placeholder="🔍 Search by Train, From or To"
+  value={search}
+  onChange={(e) => setSearch(e.target.value)}
+  className="search-box"
+/>
 
         <div className="ticket-grid">
-          {tickets.map((ticket) => (
+          {tickets
+  .filter((ticket) =>
+    ticket.trainName.toLowerCase().includes(search.toLowerCase()) ||
+    ticket.from.toLowerCase().includes(search.toLowerCase()) ||
+    ticket.to.toLowerCase().includes(search.toLowerCase())
+  )
+  .map((ticket) => (
             <div className="buy-card" key={ticket.pnr}>
               <span className="badge">{ticket.status}</span>
 
@@ -28,6 +65,12 @@ function BuyTicket() {
 <h2>₹{ticket.sellingPrice}</h2>
 
               <button>Buy Ticket</button>
+              <button
+  className="delete-btn"
+  onClick={() => handleDelete(ticket.pnr)}
+>
+  🗑 Delete Ticket
+</button>
             </div>
           ))}
         </div>
